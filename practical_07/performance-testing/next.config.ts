c23ec8cf6fb46f5d2d5c9e184e3e143327b2fa4b
Tshering Phuntsho <0223031.cst@rub.edu.bt>
2025-11-24 20:17:51 +0600

    practical 7

---
+++ practical_07/performance-testing/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.dog.ceo',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
